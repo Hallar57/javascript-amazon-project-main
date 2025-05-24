5dd5233ff4d3affaafee8783acdd22fd4d315197
Hallar57 <scentedowl1@email.com>
2025-05-24 23:12:15 +0500

    script files added

---
+++ scripts/amazon.js
@@ -0,0 +1,29 @@
+const products = [{
+  pic:'images/products/athletic-cotton-socks-6-pairs.jpg',
+  pname:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating:{
+    stars:4.5,
+    count: 87
+  },
+  priceCents: 1090
+},{
+  pic:'Intermediate Size Basketball',
+  pname:'images/products/intermediate-composite-basketball.jpg',
+  rating:{
+    stars:4,
+    count: 127
+  },
+  priceCents: 2095
+},{
+  pic:'Adults Plain Cotton T-Shirt - 2 Pack',
+  pname:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  rating:{
+    stars:4.5,
+    count: 56
+  },
+  priceCents: 2095
+}];
+
+products.forEach((product)=>{
+
+})
