285068368ef60596429ff6e878907c3bf3085bb2
Hallar57 <scentedowl1@email.com>
2025-05-27 22:16:21 +0500

    save

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,18 @@
-export let cart = [{
-    prodId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity:2
-  },
-  {
-    prodId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity:1
-  }];
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
+if (!cart) {
+  cart = [{
+    prodId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  }, {
+    prodId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addtocaart(productId){
   let matchingitem;
@@ -25,6 +31,7 @@ export function addtocaart(productId){
       quantity:1
     });
   }
+  saveToStorage();
 }
 
 export function removefromcart(prodId){
@@ -37,4 +44,5 @@ export function removefromcart(prodId){
   });
 
   cart = newcart;
+  saveToStorage();
 }
