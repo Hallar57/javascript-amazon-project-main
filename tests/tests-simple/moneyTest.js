cd4db02047ca6f81a72f221f2ce7c28071171127
Hallar57 <scentedowl1@email.com>
2025-05-29 03:14:08 +0500

    renamed tests-jasmine to test

--- tests/tests-simple/moneyTest.js
+++ tests/tests-simple/moneyTest.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from '../scripts/utils/money.js'
+import {formatCurrency} from '../../scripts/utils/money.js'
 
 console.log('1')
 formatCurrency(29934) === '299.34' ? console.log('pass') : console.log('fail');
