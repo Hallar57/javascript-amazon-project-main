c7aadfed1486f753b25effef28f4665628b75a0e
Hallar57 <scentedowl1@email.com>
2025-05-31 03:27:47 +0500

    fdsa

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,12 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
+import {loadProducsts} from '../date/products.js';
 //import '../data/cart-class.js';
-import '../data/backend-practice.js';
+//import '../data/backend-practice.js';
 
-renderOrderSummary();
-renderPaymentSummary();
-renderCheckoutHeader();
+loadProducsts(()=>{
+  renderOrderSummary();
+  renderPaymentSummary();
+  renderCheckoutHeader();
+});
