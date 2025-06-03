7fa94f51398e8ed91b733e3c8e7ff25ee977e3bd
Hallar57 <scentedowl1@gmail.com>
2025-06-04 02:06:38 +0500

    orders

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,6 +4,7 @@ import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 import {renderPaymentSummary} from './paymentSummary.js';
+import { renderCheckoutHeader } from './checkoutHeader.js';
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -129,6 +130,7 @@ export function renderOrderSummary() {
         container.remove();
 
         renderPaymentSummary();
+        //renderCheckoutHeader();
       });
     });
 
