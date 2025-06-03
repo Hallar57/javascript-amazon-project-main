948ee650379928bc843ebbadf6f48ecf5adef5d5
Hallar57 <scentedowl1@gmail.com>
2025-06-04 01:08:40 +0500

    promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,17 +2,53 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
 import {loadProducts} from '../data/products.js';
+import {loadCart} from '../data/cart.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
+Promise.all([
+  new Promise((resolve)=>{
+    loadProducts(()=>{
+      resolve();
+    });
+  }),
+  new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  })
+
+]).then(()=>{
+  renderOrderSummary();
+  renderPaymentSummary();
+  renderCheckoutHeader();
+})
+
+/*
 new Promise((resolve)=>{
   loadProducts(()=>{
     resolve();
   })
-})
 
-loadProducts(()=>{
+}).then(()=>{
+  return new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  })
+
+}).then(()=>{
   renderOrderSummary();
   renderPaymentSummary();
   renderCheckoutHeader();
-});
+})
+
+
+loadProducts(()=>{
+  loadCart(()=>{
+  renderOrderSummary();
+  renderPaymentSummary();
+  renderCheckoutHeader();
+  })
+
+});*/
